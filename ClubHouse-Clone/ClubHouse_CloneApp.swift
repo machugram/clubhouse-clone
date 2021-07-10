a7cbbd74988b1737ed4622db00994a2b07646b49
Rexford Machu <rexfordmachu@gmail.com>
2021-07-10 18:16:29 +0000

    Initial Commit

---
+++ ClubHouse-Clone/ClubHouse_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClubHouse_CloneApp.swift
+//  ClubHouse-Clone
+//
+//  Created by Rexford Machu on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ClubHouse_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
